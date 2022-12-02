25bac8762035bd0252e89101669e8dc11ba045b0
Steven Barkley <steven.barkley@outlook.com>
2022-12-02 09:04:00 -0500

    Fourth commit added print function examples

---
+++ BandNameGenerator/printing.py
@@ -0,0 +1,26 @@
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
+
+# newline character
+
+print("1.\n2.\n3.\n4.")
+
+# String concatenation
+
+print ("My age is 3" + "9")
+
+# Debug and print double quoites👇
+
+#Fix the code below 👇
+
+""" print(Day 1 - String Manipulation)
+print("String Concatenation is done with the  "+"  sign.")
+    print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.") """
+
+# Solution 
+print("Day 1 - String Manipulation")
+print("String Concatenation is done with the " '"+"' " sign.")
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
