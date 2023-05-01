a89f7105a33e78f82ea9e87f141402e7b1edcfca
Steven Barkley <steven.barkley@outlook.com>
2023-04-30 21:19:53 -0400

    Fixed weak solution for Day 2 Problem

--- TipCalculator/main.py
+++ TipCalculator/main.py
@@ -1,5 +1,11 @@
 print("Welcome to the tip calculator.")
-total_bill =input("What was the total bill?\n")
-people_count = input("How many people to split the bill?\n")
-percentage_tip = input("What percentage tip would you like toi give\n? 10, 12, or 15?")
-print("Each person should pay: %.2f" % (int(percentage_tip)*int(total_bill)/int(people_count)))
+total_bill =input("What was the total bill? ")
+billString = list(total_bill)
+cashSymbol = billString.pop(0)
+total_bill = ''.join(map(str,billString)) 
+print(total_bill)
+people_count = input("How many people to split the bill? ")
+percentage_tip = input("What percentage tip would you like toi give? 10, 12, or 15? ")
+tip_calc = float(total_bill) * float(percentage_tip) / 100
+solution = float(total_bill)/float(people_count) + tip_calc
+print("Each person should pay: " + cashSymbol + str(solution))
